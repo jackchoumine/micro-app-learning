ebe22747fb2c13850737f758884fa2dbbd135f73
ZhouQiJun <jackchou4job@163.com>
2024-01-11 17:17:00 +0800

    应用生命周期

--- vue3-host-app/src/main.js
+++ vue3-host-app/src/main.js
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-01-11 16:51:33
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-01-11 17:00:05
+ * @LastEditTime: 2024-01-11 17:16:24
  * @Description :
  */
 import { createApp } from 'vue'
@@ -10,6 +10,24 @@ import './style.css'
 import App from './App.vue'
 import microApp from '@micro-zoe/micro-app'
 
-microApp.start()
+microApp.start({
+  lifeCycles: {
+    created(e) {
+      console.log('created')
+    },
+    beforemount(e) {
+      console.log('beforemount')
+    },
+    mounted(e) {
+      console.log('mounted')
+    },
+    unmount(e) {
+      console.log('unmount')
+    },
+    error(e) {
+      console.log('error')
+    },
+  },
+})
 
 createApp(App).mount('#app')
