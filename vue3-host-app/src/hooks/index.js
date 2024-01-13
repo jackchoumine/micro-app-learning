212b3dbe75ee622c2698e3c442728cf14f88b57a
jack <jackzhoumine@gmail.com>
2024-01-14 02:43:45 +0800

    feat: 完善 useLocalStorage

--- vue3-host-app/src/hooks/index.js
+++ vue3-host-app/src/hooks/index.js
@@ -2,12 +2,15 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-01-13 23:57:12
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-01-14 00:53:51
+ * @LastEditTime: 2024-01-14 02:39:51
  * @Description :
  */
-import { customRef } from 'vue'
+import { customRef, nextTick } from 'vue'
+// import { createGlobalState } from '@vueuse/core'
 
-export function useLocalStorage(key, initialValue = null) {
+export const useLocalStorage = localStorage()
+
+function _useLocalStorage(key, initialValue = null) {
   const refValue = customRef((track, trigger) => {
     return {
       // 获取数据值
@@ -15,7 +18,7 @@ export function useLocalStorage(key, initialValue = null) {
         track()
         let val = null
         try {
-          val = JSON.parse(localStorage.getItem(key))
+          val = JSON.parse(window.localStorage.getItem(key))
           // return val
         } catch (error) {
           //   return null
@@ -23,17 +26,44 @@ export function useLocalStorage(key, initialValue = null) {
         if (val) return val
         const strValue = JSON.stringify(initialValue)
         // 把初始化的值存进去
-        localStorage.setItem(key, strValue)
+        window.localStorage.setItem(key, strValue)
         return initialValue
       },
       // 监听数据变化
       set: newVal => {
+        console.log('set', newVal)
         trigger()
         const strValue = JSON.stringify(newVal)
         // console.log('set', newVal, strValue)
-        localStorage.setItem(key, strValue)
+        window.localStorage.setItem(key, strValue)
       },
     }
   })
   return refValue
+}
+
+function localStorage() {
+  const map = new Map()
+
+  function removeItem(key) {
+    if (!key) return false
+    if (!map.has(key)) return false
+    const [state] = map.get(key)
+    state.value = null
+    map.delete(key)
+    nextTick(() => {
+      window.localStorage.removeItem(key)
+    })
+    return true
+  }
+
+  return (key, initialValue) => {
+    if (!map.has(key)) {
+      const state = _useLocalStorage(key, initialValue)
+      const remove = () => removeItem(key)
+      map.set(key, [state, remove])
+      return [state, remove]
+    }
+    return map.get(key)
+  }
 }
