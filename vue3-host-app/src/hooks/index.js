ce7b85a7545e9a2a7008d8a0e1cf8e26f243b2df
jack <jackzhoumine@gmail.com>
2024-01-14 03:30:39 +0800

    fixed: 修复移除后再次设置值，页面不更新

--- vue3-host-app/src/hooks/index.js
+++ vue3-host-app/src/hooks/index.js
@@ -2,12 +2,14 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-01-13 23:57:12
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-01-14 02:39:51
+ * @LastEditTime: 2024-01-14 03:29:06
  * @Description :
  */
 import { customRef, nextTick } from 'vue'
 // import { createGlobalState } from '@vueuse/core'
 
+// example
+// const [count,removeCount] = useLocalStorage('count',0)
 export const useLocalStorage = localStorage()
 
 function _useLocalStorage(key, initialValue = null) {
@@ -23,19 +25,22 @@ function _useLocalStorage(key, initialValue = null) {
         } catch (error) {
           //   return null
         }
+        // debugger
         if (val) return val
         const strValue = JSON.stringify(initialValue)
         // 把初始化的值存进去
         window.localStorage.setItem(key, strValue)
+        // debugger
+        console.log('get', strValue)
         return initialValue
       },
       // 监听数据变化
       set: newVal => {
         console.log('set', newVal)
-        trigger()
         const strValue = JSON.stringify(newVal)
         // console.log('set', newVal, strValue)
         window.localStorage.setItem(key, strValue)
+        trigger()
       },
     }
   })
@@ -47,18 +52,20 @@ function localStorage() {
 
   function removeItem(key) {
     if (!key) return false
-    if (!map.has(key)) return false
+    if (window.localStorage.getItem(key) === null) return false
     const [state] = map.get(key)
     state.value = null
-    map.delete(key)
+    // NOTE 不要删除 key 否则再次设置值后，无法移除
+    // map.delete(key)
     nextTick(() => {
       window.localStorage.removeItem(key)
+      //  console.log('removeItem', key)
     })
     return true
   }
 
   return (key, initialValue) => {
-    if (!map.has(key)) {
+    if (!map.get(key)) {
       const state = _useLocalStorage(key, initialValue)
       const remove = () => removeItem(key)
       map.set(key, [state, remove])
